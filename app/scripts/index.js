2455ae7d94c000101d2fa1f2f6dd8928cd605459
Nathan <nathanstarwalt@gmail.com>
2017-01-31 14:13:32 -0500

    HTML skeleton

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,1 +1,38 @@
-console.log("Hello World!");
+var $ = require("jquery");
+var _ = require("underscore");
+var handlebars = require("handlebars");
+var url = "https://api.etsy.com/v2/listings/active.js?api_key=cdwxq4soa7q4zuavbtynj8wx&keywords=magic+the+gathering&includes=Images,Shop&sort_on=score";
+
+/*
+  (url: String, callback: Function) -> undefined
+
+  Execute a callback function with the JSON results from the url specified.
+
+  Examples
+      var url = "https://api.etsy.com/v2/listings/active.js?api_key=cdwxq4soa7q4zuavbtynj8wx&keywords=yarn&includes=Images,Shop";
+
+      fetchJSONP(url, function(data) {
+        // do something with data
+      });
+
+      // OR
+
+      function logData(data) {
+        console.log(data);
+      }
+
+      fetchJSONP(url, logData);
+*/
+function fetchJSONP(url, callback) {
+    var callbackName = 'jsonp_callback_' + Math.round(100000 * Math.random());
+    var script = document.createElement('script');
+
+    window[callbackName] = function(data) {
+        delete window[callbackName];
+        document.body.removeChild(script);
+        callback(data);
+    };
+
+    script.src = url + (url.indexOf('?') >= 0 ? '&' : '?') + 'callback=' + callbackName;
+    document.body.appendChild(script);
+}
