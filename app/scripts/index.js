d15b71aeda70778872764bc72a77f0f6e9bf0ddd
Nathan <nathanstarwalt@gmail.com>
2017-01-31 16:59:52 -0500

    Search Returns showing

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,6 +1,8 @@
+// Setting up jQuery, Underscore and Handlebars
 var $ = require("jquery");
 var _ = require("underscore");
 var handlebars = require("handlebars");
+// Search for Magic The Gathering
 var url = "https://api.etsy.com/v2/listings/active.js?api_key=cdwxq4soa7q4zuavbtynj8wx&keywords=magic+the+gathering&includes=Images,Shop&sort_on=score";
 
 /*
@@ -36,3 +38,22 @@ function fetchJSONP(url, callback) {
     script.src = url + (url.indexOf('?') >= 0 ? '&' : '?') + 'callback=' + callbackName;
     document.body.appendChild(script);
 }
+//sets up template for handlebars
+var itemProfile = $('#item-profile').html();
+var template = handlebars.compile(itemProfile);
+
+var $searchReturn = $(".search-return");
+
+//creates the searched items
+fetchJSONP(url, function(data) {
+  _.each(data.results, function(items){
+    var specifics = {
+      picture: items.Images[0].url_170x135,
+      itemName: items.title,
+      creator: items.Shop.shop_name,
+      price: items.price
+    };
+    $searchReturn.append(template(specifics));
+  });
+
+});
